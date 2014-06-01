c1b02fb0ede1fc5b340a3d445a37f3363c74f6f3
Bernhard Pieber <bernhard@pieber.com>
2014-06-01 22:54:04 +0200

    Make mvn -Pclient work again, opens minimal Swing GUI

--- src/main/java/swa/ass4/client/Client.java
+++ src/main/java/swa/ass4/client/Client.java
@@ -1,79 +1,46 @@
 package swa.ass4.client;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-public class Client extends Application {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
 
-	@Override
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("JavaFX Welcome");
-		GridPane grid = new GridPane();
-		grid.setAlignment(Pos.CENTER);
-		grid.setHgap(10);
-		grid.setVgap(10);
-		grid.setPadding(new Insets(25, 25, 25, 25));
+import swa.ass4.server.cxf.LoginService;
 
-		Text scenetitle = new Text("Welcome");
-		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-		grid.add(scenetitle, 0, 0, 2, 1);
+public class Client {
+	public static void main(String args[]) throws Exception {
+		// LoginService loginService = getLoginService();
+		// User currentUser = loginService.login("admin", "password");
 
-		Label userName = new Label("User Name:");
-		grid.add(userName, 0, 1);
-
-		TextField userTextField = new TextField();
-		grid.add(userTextField, 1, 1);
-
-		Label pw = new Label("Password:");
-		grid.add(pw, 0, 2);
-
-		PasswordField pwBox = new PasswordField();
-		grid.add(pwBox, 1, 2);
-
-		Button btn = new Button("Sign in");
-		HBox hbBtn = new HBox(10);
-		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-		hbBtn.getChildren().add(btn);
-		grid.add(hbBtn, 1, 4);
-
-		final Text actiontarget = new Text();
-		grid.add(actiontarget, 0, 6);
-		grid.setColumnSpan(actiontarget, 2);
-		grid.setHalignment(actiontarget, HPos.RIGHT);
-		actiontarget.setId("actiontarget");
-
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent e) {
-				actiontarget.setFill(Color.FIREBRICK);
-				actiontarget.setText("Sign in button pressed");
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowLoginGUI();
 			}
 		});
-
-		Scene scene = new Scene(grid, 300, 275);
-		primaryStage.setScene(scene);
-		primaryStage.show();
 	}
 
-	public static void main(String[] args) {
-		launch(args);
+	private static LoginService getLoginService() throws MalformedURLException {
+		URL wsdlURL = new URL("http://localhost:9000/LoginService?wsdl");
+		QName SERVICE_NAME = new QName("http://cxf.server.ass4.swa/",
+				"LoginServiceImplService");
+		Service service = Service.create(wsdlURL, SERVICE_NAME);
+		LoginService loginService = service.getPort(LoginService.class);
+		return loginService;
 	}
 
+	private static void createAndShowLoginGUI() {
+		JFrame frame = new JFrame("University Grading System – Login");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JLabel userNameLabel = new JLabel("Username");
+		frame.getContentPane().add(userNameLabel);
+		JLabel passwordLabel = new JLabel("Password");
+		frame.getContentPane().add(passwordLabel);
+
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
